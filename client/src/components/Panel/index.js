1db69ef183e612f5cc954acb39de6f0675ffbdb7
Gavin56 <gavinobrien56@gmail.com>
2021-02-06 13:53:17 -0500

    Spotify rendered in right panel.

--- client/src/components/Panel/index.js
+++ client/src/components/Panel/index.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    overflow: 'hidden',
-    flexWrap: 'noWrap',
-    '& > *': {
+    display: "flex",
+    overflow: "hidden",
+    flexWrap: "noWrap",
+    "& > *": {
       margin: theme.spacing(7),
       width: "100%",
       height: theme.spacing(50),
-      paddingBottom: theme.spacing(14.5)
+      paddingBottom: theme.spacing(14.5),
     },
   },
 }));
 
-export default function SimplePaper() {
+export default function Panel(props) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Paper style={{backgroundColor:"#4a4a4a"}} elevation={3} />
+      <Paper style={{ backgroundColor: "#4a4a4a" }} elevation={3}>
+        {props.children}
+      </Paper>
     </div>
   );
 }
