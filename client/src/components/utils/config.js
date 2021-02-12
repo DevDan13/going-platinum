5365dfffc254ad636d2c49263f18558bef400e64
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2021-02-11 20:28:48 -0500

    commiting changes for trying to get .env to hide client id for merge.

--- client/src/components/utils/config.js
+++ client/src/components/utils/config.js
@@ -1,7 +1,11 @@
+// import React from "react";
+// import env from "react-dotenv";
+const dotenv = require("dotenv").config();
+
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = "f6d9e1e4ff1b4415ab6e0a54460b7bdf";
+export const clientId = process.env.CLIENT_ID;
 export const redirectUri = "http://localhost:3000/callback/";
 export const scopes = [
     "user-top-read",
