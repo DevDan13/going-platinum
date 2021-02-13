51eb19437ae4b907ab7320ca5ea429bfb500f458
Ibrameias <ibramdarwish@gmail.com>
2021-02-13 11:58:46 -0500

    added login page

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Form from "../../components/loginForm/Form"
+
+
+
+
+function Login (){
+    return (
+        <div>
+           <div className="img">
+           <Form/>
+           </div> 
+            
+        </div>
+    )
+}
+
+export default Login;
