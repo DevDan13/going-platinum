3073d7934d74ffc428ce4f4c4f4612f5f3db5e93
Amanda D'Argenio <dargenioa14@gmail.com>
2021-02-13 17:43:34 -0500

    added spotify api route to handle auth on backend

--- client/src/components/Firebase/SignIn.js
+++ client/src/components/Firebase/SignIn.js
@@ -1,31 +1,38 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase";
 
 const SignIn = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
-    const signInWithEmailAndPasswordHandler = 
-            (event,email, password) => {
-                event.preventDefault();
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const signInWithEmailAndPasswordHandler = (event, email, password) => {
+    event.preventDefault();
+    auth.signInWithEmailAndPassword(email, password).catch((error) => {
+      setError("Error signing in with password and email!");
+      console.error("Error signing in with password and email", error);
+    });
+  };
 
-      const onChangeHandler = (event) => {
-          const {name, value} = event.currentTarget;
+  const onChangeHandler = (event) => {
+    const { name, value } = event.currentTarget;
 
-          if(name === 'userEmail') {
-              setEmail(value);
-          }
-          else if(name === 'userPassword'){
-            setPassword(value);
-          }
-      };
+    if (name === "userEmail") {
+      setEmail(value);
+    } else if (name === "userPassword") {
+      setPassword(value);
+    }
+  };
 
   return (
     <div className="mt-8">
       <h1 className="text-3xl mb-2 text-center font-bold">Sign In</h1>
       <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
-        {error !== null && <div className = "py-4 bg-red-600 w-full text-white text-center mb-3">{error}</div>}
+        {error !== null && (
+          <div className="py-4 bg-red-600 w-full text-white text-center mb-3">
+            {error}
+          </div>
+        )}
         <form className="">
           <label htmlFor="userEmail" className="block">
             Email:
@@ -34,10 +41,10 @@ const SignIn = () => {
             type="email"
             className="my-1 p-1 w-full"
             name="userEmail"
-            value = {email}
+            value={email}
             placeholder="E.g: faruq123@gmail.com"
             id="userEmail"
-            onChange = {(event) => onChangeHandler(event)}
+            onChange={(event) => onChangeHandler(event)}
           />
           <label htmlFor="userPassword" className="block">
             Password:
@@ -46,18 +53,22 @@ const SignIn = () => {
             type="password"
             className="mt-1 mb-3 p-1 w-full"
             name="userPassword"
-            value = {password}
+            value={password}
             placeholder="Your Password"
             id="userPassword"
-            onChange = {(event) => onChangeHandler(event)}
+            onChange={(event) => onChangeHandler(event)}
           />
-          <button className="bg-green-400 hover:bg-green-500 w-full py-2 text-white" onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
+          <button
+            className="bg-green-400 hover:bg-green-500 w-full py-2 text-white"
+            onClick={(event) => {
+              signInWithEmailAndPasswordHandler(event, email, password);
+            }}
+          >
             Sign in
           </button>
         </form>
         <p className="text-center my-3">or</p>
-        <button
-          className="bg-red-500 hover:bg-red-600 w-full py-2 text-white">
+        <button className="bg-red-500 hover:bg-red-600 w-full py-2 text-white">
           Sign in with Google
         </button>
         <p className="text-center my-3">
@@ -66,7 +77,10 @@ const SignIn = () => {
             Sign up here
           </Link>{" "}
           <br />{" "}
-          <Link to = "passwordReset" className="text-blue-500 hover:text-blue-600">
+          <Link
+            to="passwordReset"
+            className="text-blue-500 hover:text-blue-600"
+          >
             Forgot Password?
           </Link>
         </p>
