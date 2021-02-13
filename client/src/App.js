bcc9895b96b1fa102f7b835918335b5d54877ee8
Ibrameias <ibramdarwish@gmail.com>
2021-02-13 13:33:15 -0500

    fixed path

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import Home from "../src/pages/Home /Home"
-import Login from "../src/pages/Login/Login";
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login";
+import Profile from "./pages/profile";
+import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import './App.css';
-import Navbar from "./components/Navbar/Navbar";
-import Profile from "../src/pages/profile/index"
-
 function App() {
   return (
     <div className="App">
