33137b1cbeb3a3130d682db9a8b61a996c61f863
Ibrameias <ibramdarwish@gmail.com>
2021-02-13 11:57:38 -0500

    added home page

---
+++ client/src/pages/Home /Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import Hero from "../../components/Hero";
+// import Container from "../../components/Container";
+// import Row from "../../components/Row";
+// import Col from "../../components//Col";
+import Btn from "../../components/Btn/index"
+import "./style.css"
+
+
+
+function Home() {
+    return (
+        <div>
+            <div className="img">
+                <Btn />
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
