1d30fb8d9f036bd719ced7c0d068bb17454e54b4
Amanda D'Argenio <dargenioa14@gmail.com>
2021-02-13 12:49:38 -0500

    firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import Header from "./components/Header/index";
-// import Panel from "./components/Panel/index";
 import Home from "./pages/Home/index";
-// import Grid from "./components/Grid/index";
-
+import Application from "./components/Firebase/Application";
 
 
 function App() {
---
+++ client/src/components/Firebase/Application.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import ProfilePage from "./ProfilePage";
+import PasswordReset from "./PasswordReset";
+
+function Application() {
+    const user = null;
+    return (
+          user ?
+          <ProfilePage />
+        :
+          <Router>
+            <SignUp path="signUp" />
+            <SignIn path="/" />
+            <PasswordReset path = "passwordReset" />
+          </Router>
+  
+    );
+  }
+  export default Application;
+  
