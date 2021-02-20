e2a99b1d27892c963923dba8b4a982926dd0ed6c
Gavin56 <gavinobrien56@gmail.com>
2021-02-20 00:29:27 -0500

    New Login component with CSS.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import React, { useEffect } from "react";
 // import Row from "../../components/Row";
 // import Col from "../../components//Col";
 import Btn from "../../components/Btn/index";
-import Spotify from "../../components/video/spotify.mp4"
+import Spotify from "../../components/video/spotify.mp4";
 import "./style.css";
 import API from "../../utils/API";
 
@@ -20,7 +20,6 @@ function Home() {
     API.getSpotifyRecommendations().then((res) => {
       console.log(res.data);
       //items.id
-
     });
   };
   // const testToken = () => {
@@ -41,11 +40,16 @@ function Home() {
 
   return (
     <div>
-      <div >
-        <video className="video" autoPlay="autoplay" loop="loop" muted  >
+      <div>
+        <video className="video" autoPlay="autoplay" loop="loop" muted>
           <source src={Spotify} type="video/mp4" />
+          <h1 className="listening">
+            Music is
+            <br />
+            everything
+          </h1>
         </video>
-        <h1 className="listening">Music is<br />everything</h1>
+
         <button onClick={testBtn}>Test</button>
         <button onClick={getPlaylist}>playlist</button>
         <Btn />
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -2,14 +2,16 @@ import React from "react";
 import FormControl from "@material-ui/core/FormControl";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import LoginForm from "../../components/LoginForm/Form"
 
 function Login() {
   return (
     <div>
       <div className="img">
-        <Paper>
+        <LoginForm></LoginForm>
+        {/* <Paper>
           <FormControl />
-        </Paper>
+        </Paper> */}
       </div>
     </div>
   );
