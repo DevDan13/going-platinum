2251ce861f2fa0f1f65feabc117cc9a90f9c1f19
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2021-02-23 19:06:23 -0500

    added create to user controller.

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,5 +5,11 @@ module.exports = {
         db.User.findById(req.params.id)
       .then(dbUser => res.json(dbUser))
       .catch(err => res.status(422).json(err));
-    }
+    },
+
+    create: function(req, res) {
+      db.User.create(req.body)
+        .then(dbUser => res.json(dbUser))
+        .catch(err => res.status(422).json(err))
+    },
 }
