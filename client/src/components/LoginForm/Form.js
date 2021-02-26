90f55ced5524e0e18ec962071e7ac5b53e542d6e
Gavin56 <gavinobrien56@gmail.com>
2021-02-26 13:17:59 -0500

    Added PulsePlayer and mobile responsiveness to login page.

--- client/src/components/LoginForm/Form.js
+++ client/src/components/LoginForm/Form.js
@@ -4,20 +4,22 @@ import SignIn from "../Firebase/SignIn";
 import Grid from "@material-ui/core/Grid";
 import Header from "../Header/index";
 import Footer from "../Footer/index";
-import PlayerPulse from "../PlayerPulse/index"
+import PlayerPulse from "../PlayerPulse/index";
 
 function LoginForm() {
   return (
     <div className="img">
       {/* <Header></Header> */}
       <Grid container id="grid-container">
-        <Grid id="signin-div" item xs={12} md={6}>
+        <Grid id="signin-div" item xs={12} md={5}>
           <SignIn />
         </Grid>
 
-        {/* <PlayerPulse></PlayerPulse> */}
+        <Grid id="motion-media-div" style={{display:"flex", justifyContent: "center"}}item xs={12} md={2}>
+          <PlayerPulse playing={true} size={"la-3x"}></PlayerPulse>
+        </Grid>
 
-        <Grid id="signup-div" item xs={12} md={6}>
+        <Grid id="signup-div" item xs={12} md={5}>
           <SignUp />
         </Grid>
       </Grid>
