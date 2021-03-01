06976cf0af9ab54cb77b67daa5356152199db0f6
Gavin56 <gavinobrien56@gmail.com>
2021-03-01 14:01:03 -0500

    Mario pipe added to Profile404.

--- client/src/pages/Profile404/index.js
+++ client/src/pages/Profile404/index.js
@@ -1,22 +1,20 @@
 import Slide from "@material-ui/core/Slide";
 import "./style.css";
+import "./style.scss";
 
 function Profile404() {
-  const directionArray = ["left", "right", "up", "down"];
-  let randomIndex = Math.floor(Math.random() * directionArray.length);
-  let direction = directionArray[randomIndex];
   return (
     <div id="profile404-background">
       <div id="profile404-text">
-        <h1 style={{ fontFamily: "DotGothic16", fontSize: 55 }}>404</h1>
-        <h2 style={{ fontFamily: "DotGothic16", fontSize: 35 }}>
+        <h1 style={{ fontFamily: "DotGothic16"}}>404</h1>
+        <h2 style={{ fontFamily: "DotGothic16"}}>
           PAGE NOT FOUND.
         </h2>
         <p style={{ fontFamily: "DotGothic16", fontSize: 20 }}>
           Your page is in another castle!
         </p>
         <Slide
-          direction={direction}
+          direction={"up"}
           in={true}
           {...{ timeout: 1000 }}
           mountOnEnter
@@ -31,6 +29,9 @@ function Profile404() {
         <p style={{ fontFamily: "DotGothic16", fontSize: 20 }}>
           Make sure you're logged in before attempting to view this page!
         </p>
+      </div>
+      <div id="pipe">
+        <a href="/login" class="eightbit-btn eightbit-btn--reset">Go Back</a>
       </div>
     </div>
   );
