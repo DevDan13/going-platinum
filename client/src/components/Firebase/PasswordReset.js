9a50b49ca0f4ec74e6ea730d44cf589175865857
Ibrameias <ibramdarwish@gmail.com>
2021-03-01 21:29:01 -0500

    crated forgot password page

--- client/src/components/Firebase/PasswordReset.js
+++ client/src/components/Firebase/PasswordReset.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase";
+// import "./Reset.css"
+// import ResetBtn from "../../components/Reset/"
 
 const PasswordReset = () => {
   const [email, setEmail] = useState("");
   const [emailHasBeenSent, setEmailHasBeenSent] = useState(false);
   const [error, setError] = useState(null);
-  
+
   const onChangeHandler = (event) => {
     const { name, value } = event.currentTarget;
     if (name === "userEmail") {
@@ -30,48 +32,92 @@ const PasswordReset = () => {
   };
 
   return (
-    <div className="mt-8">
-      <h1 className="text-xl text-center font-bold mb-3">
-        Reset your Password
-      </h1>
-      <div className="border border-blue-300 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
-        <form action="">
-          {emailHasBeenSent && (
-            <div className="py-3 bg-green-400 w-full text-white text-center mb-3">
-              An email has been sent to you!
-            </div>
-          )}
-          {error !== null && (
-            <div className="py-3 bg-red-600 w-full text-white text-center mb-3">
-              {error}
-            </div>
-          )}
-          <label htmlFor="userEmail" className="w-full block">
-            Email:
-          </label>
-          <input
-            type="email"
-            name="userEmail"
-            id="userEmail"
-            value={email}
-            placeholder="Input your email"
-            onChange={onChangeHandler}
-            className="mb-3 w-full px-1 py-2"
-          />
-          <button
-          onClick={sendResetEmail}
-          className="w-full bg-blue-400 text-white py-3">
-            Send me a reset link
-          </button>
-        </form>
-        <Link
-          to="/"
-          className="my-2 text-blue-700 hover:text-blue-800 text-center block"
-        >
-          &larr; back to sign in page
-        </Link>
-      </div>
+    // <div>
+    //   <div className="mt-8">
+    //     <h1 className="text-xl text-center font-bold mb-3">
+    //       Reset your Password
+    //    </h1>
+    //     <div className="border border-blue-300 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
+    //       <form action="">
+    //         {emailHasBeenSent && (
+    //           <div className="py-3 bg-green-400 w-full text-white text-center mb-3">
+    //             An email has been sent to you!
+    //           </div>
+    //         )}
+    //         {error !== null && (
+    //           <div className="py-3 bg-red-600 w-full text-white text-center mb-3">
+    //             {error}
+    //           </div>
+    //         )}
+    //         <label htmlFor="userEmail" className="w-full block">
+    //           Email:
+    //        </label>
+    //         <input
+    //           type="email"
+    //           name="userEmail"
+    //           id="userEmail"
+    //           value={email}
+    //           placeholder="Input your email"
+    //           onChange={onChangeHandler}
+    //           className="mb-3 w-full px-1 py-2"
+    //         />
+    //         <button
+    //           onClick={sendResetEmail}
+    //           className="w-full bg-blue-400 text-white py-3">
+    //           Send me a reset link
+    //        </button>
+    //       </form>
+    //       <Link
+    //         to="/"
+    //         className="my-2 text-blue-700 hover:text-blue-800 text-center block"
+    //       >
+    //         &larr; back to sign in page
+    //      </Link>
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+      <form action="">
+        {emailHasBeenSent && (
+          <div className="py-3 bg-green-400 w-full text-white text-center mb-3">
+            An email has been sent to you!
+          </div>
+        )}
+        {error !== null && (
+          <div className="py-3 bg-red-600 w-full text-white text-center mb-3">
+            {error}
+          </div>
+        )}
+
+        <div className="container" onclick="onclick">
+          <div className="top"></div>
+          <div className="bottom"></div>
+          <div className="center">
+            <h2>Forgot Password!</h2>
+            <input type="email" placeholder="Your e-mail" name="userEmail" id="userEmail" value={email} onChange={onChangeHandler} />
+            <button onClick={sendResetEmail}
+            className="btn btn-bubble">Send</button>
+            <h2>&nbsp;</h2>
+            <Link
+        to="/login"
+        // className="my-2 text-blue-700 hover:text-blue-800 text-center block"
+      >
+         Try Again
+         </Link>
+          </div>
+        </div>
+      </form>
     </div>
+
   );
 };
 export default PasswordReset;
+
+
+
+
+
+
+
+
+
