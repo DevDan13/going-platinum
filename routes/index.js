963cf280a6b650f6baabe9fe9cf1c29465dc1940
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2021-03-04 18:10:02 -0500

    fixing rputes for heroku.

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 //If no Api routes Hit send react app
-router.use(function* (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 module.exports = router;
