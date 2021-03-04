6893b40635cf9fdbc9d72da22e58986ca4d97c1c
Gavin56 <gavinobrien56@gmail.com>
2021-03-03 20:39:41 -0500

    Added redirect to Google signin.

--- client/src/components/Firebase/SignIn.js
+++ client/src/components/Firebase/SignIn.js
@@ -38,6 +38,12 @@ const SignIn = () => {
     }
   };
 
+  auth.onAuthStateChanged(function(currentUser) {
+    if (currentUser) {
+      history.push("/profile")
+    } 
+  });
+
   const popover = (
     <Popover
       id="popover-basic"
